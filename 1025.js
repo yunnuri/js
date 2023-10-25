216f8e2ce5cf381ece0e943bb67d3cb13dc6eb82
yunnuri <133933579+yunnuri@users.noreply.github.com>
2023-10-25 12:45:09 +0900

    Update 1025.js

--- 1025.js
+++ 1025.js
@@ -3,3 +3,26 @@
     const result = fruits.join(',');
     console.log(result);
 }
+
+{
+    const array = [1,2,3,4,5];
+    const result = array.slice(2,5);
+    console.log(result);
+    console.log(array);
+}
+
+class student {
+    consttructor (name,age,enrolled.score) {
+        this.name = name;
+        this.age = age;
+        this.enrolled = enrolled;
+        this.score = score;
+    }
+}
+const students = {
+    new student('a',29, true, 45),
+    new student('b',28, false, 80),
+    new student('c',30, true, 90),
+    new student('d',40, false, 66),
+    new student('e',18, true, 88),
+}
